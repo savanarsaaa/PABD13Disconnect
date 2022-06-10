aa3fcf000653aa3ec61498b0163b04e1d0c7b037
Savanarsaaa <97094113+savanarsaaa@users.noreply.github.com>
2022-06-11 03:07:26 +0700

    First Commit

---
+++ Disconnected/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Disconnected
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt;
+        DataRow dr;
+        String code;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
+
+            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
+            txtCode.Enabled = false;
+            txtName.Enabled = false;
+            txtAddress.Enabled = false;
+            txtState.Enabled = false;
+            txtCountry.Enabled = false;
+            cbDesignation.Enabled = false;
+            cbDepartment.Enabled = false;
+            cbDesignation.Items.Add("MANAGER");
+            cbDesignation.Items.Add("AUTHOR");
+            cbDesignation.Items.Add("Designer");
+            cbDepartment.Items.Add("MARKETING");
+            cbDepartment.Items.Add("FINANCE");
+            cbDepartment.Items.Add("IDD");
+            cmdSave.Enabled = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'hRDataSet.empdetails' table. You can move, or remove it, as needed.
+            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
+
+        }
+
+        private void bindingNavigator1_RefreshItems(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmdSave.Enabled = true;
+            txtName.Enabled = true;
+            txtAddress.Enabled = true;
+            txtState.Enabled = true;
+            txtCountry.Enabled = true;
+            cbDesignation.Enabled = true;
+            cbDepartment.Enabled = true;
+            txtName.Text = "";
+            txtAddress.Text = "";
+            txtState.Text = "";
+            txtCountry.Text = "";
+            cbDesignation.Text = "";
+            cbDepartment.Text = "";
+            int ctr, len;
+            string codeval;
+            dt = hRDataSet.Tables["empdetails"];
+            len = dt.Rows.Count - 1;
+            dr = dt.Rows[len];
+            code = dr["ccode"].ToString();
+            codeval = code.Substring(1, 3);
+            ctr = Convert.ToInt32(codeval);
+            if ((ctr >= 1) && (ctr < 9))
+            {
+                ctr = ctr + 1;
+                txtCode.Text = "C00" + ctr;
+            }
+            else if ((ctr >= 9) && (ctr < 99))
+            {
+                ctr = ctr + 1;
+                txtCode.Text = "C0" + ctr;
+            }
+            else if (ctr >= 99)
+            {
+                ctr = ctr + 1;
+                txtCode.Text = "C" + ctr;
+            }
+
+            cmdAdd.Enabled = false;
+
+        }
+
+        private void cmdSave_Click(object sender, EventArgs e)
+        {
+            dt = hRDataSet.Tables["empdetails"];
+            dr = dt.NewRow();
+            dr[0] = txtCode.Text;
+            dr[1] = txtName.Text;
+            dr[2] = txtAddress.Text;
+            dr[3] = txtState.Text;
+            dr[4] = txtCountry.Text;
+            dr[5] = cbDesignation.SelectedItem;
+            dr[6] = cbDepartment.SelectedItem;
+            dt.Rows.Add(dr);
+            empdetailsTableAdapter.Update(hRDataSet);
+            txtCode.Text = System.Convert.ToString(dr[0]);
+            txtCode.Enabled = false;
+            txtName.Enabled = false;
+            txtAddress.Enabled = false;
+            txtState.Enabled = false;
+            txtCountry.Enabled = false;
+            cbDesignation.Enabled = false;
+            cbDepartment.Enabled = false;
+            this.empdetailsTableAdapter.Fill(this.hRDataSet.empdetails);
+            cmdAdd.Enabled = true;
+            cmdSave.Enabled = false;
+
+        }
+
+        private void cmdDelete_Click(object sender, EventArgs e)
+        {
+            string code;
+            code = txtCode.Text;
+            dr = hRDataSet.Tables["empdetails"].Rows.Find(code);
+            dr.Delete();
+            empdetailsTableAdapter.Update(hRDataSet);
+
+        }
+    }
+}
